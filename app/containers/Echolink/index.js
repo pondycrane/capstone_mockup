c58f2a09bf900de8eec2920a93eb831399371c3d
Hank Huang <pondycrane@gmail.com>
2018-02-22 14:17:14 -0500

    first commit

---
+++ app/containers/Echolink/index.js
@@ -0,0 +1,19 @@
+/**
+ * Echolink
+ *
+ * This is the Echolink test page.
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Echolink extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <p>This is Echolink yeah man</p>
+      </div>
+    );
+  }
+}
+
+export default Echolink;
